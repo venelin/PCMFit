a00eff778fa58b4cb1cfa464ea94d563ef387783
Venelin Mitov <venelin.mitov@intiquan.com>
2021-06-05 20:53:53 +0200

    Edited bootstrap vignette.

--- vignettes/FitBootstrap_MGPM_A_F_BC2_RR.R
+++ vignettes/FitBootstrap_MGPM_A_F_BC2_RR.R
@@ -1,3 +1,5 @@
+# File: FitBootstrap_MGPM_A_F_BC2_RR.R
+# Usage: R --vanilla --slave -f ../../FitBootstrap_MGPM_A_F_BC2_RR.R --args 1
 library(PCMBase)
 library(PCMBaseCpp)
 library(PCMFit)
@@ -14,38 +16,56 @@ if(length(args) > 0) {
 # A character string used in filenames for a model inference on a given data:
 prefixFiles = paste0("MGPM_A_F_BC2_RR_BSID_", data_id)
 
-# creating the cluster for this PCMFit run:
-if(!exists("cluster") || is.null(cluster)) {
-  if(require(doMPI)) {
-    # using MPI cluster as distributed node cluster (possibly running on a
-    # cluster of multiple nodes)
-    # Get the number of cores. Assume this is run in a batch job.
-    p = strtoi(Sys.getenv('LSB_DJOB_NUMPROC'))
-    cluster <- startMPIcluster(count = p-1, verbose = TRUE)
-    doMPI::registerDoMPI(cluster)
-  } else {
-    # possibly running on personal computer without mpi installation
-    cluster <- parallel::makeCluster(
-      parallel::detectCores(logical = TRUE),
-      outfile = paste0("log_", prefixFiles, ".txt"))
-    doParallel::registerDoParallel(cluster)
-  }
-}
+# Creating the cluster for this PCMFit run.
+# Uncomment the follwing code only if doMPI package is available and working.
+# if(!exists("cluster") || is.null(cluster)) {
+#   if(require(doMPI)) {
+#     # using MPI cluster as distributed node cluster (possibly running on a
+#     # cluster of multiple nodes)
+#     # Get the number of cores. Assume this is run in a batch job.
+#     p = strtoi(Sys.getenv('LSB_DJOB_NUMPROC'))
+#     cluster <- startMPIcluster(count = p-1, verbose = TRUE)
+#     doMPI::registerDoMPI(cluster)
+#   } else {
+#     # possibly running on personal computer without mpi installation
+#     cluster <- parallel::makeCluster(
+#       parallel::detectCores(logical = TRUE),
+#       outfile = paste0("log_", prefixFiles, ".txt"))
+#     doParallel::registerDoParallel(cluster)
+#   }
+#}
 
 # This function is going to be executed on each worker node.
+# Hence, it is the convenient place to define global settings,
+# in particular:
+# - set global option values,
+# - call the function PCMGenerateModelTypes to generate the default PCM model types;
+# - write custom S3 methods for PCMBase::PCMParentClasses and PCMBase::PCMSpecify
+# for custom PCM model types (see corresponding man pages in the PCMBase package),
+# - define S3 methods for the PCMBase::PCMParamUpperLimit and PCMParamLowerLimit in the
+# global environment (see R-code below).
 generatePCMModelsFunction <- function() {
-  # make results reproducible
-  set.seed(4, kind = "Mersenne-Twister", normal.kind = "Inversion")
+  # make results reproducible but keep in mind that data_id is unique for this specific
+  # worker node, and will be used to generate a unique parametric boostrap set of trait values.
+  set.seed(data_id, kind = "Mersenne-Twister", normal.kind = "Inversion")
 
   PCMGenerateModelTypes()
-  fileName <- '../../DefineParameterLimits.R'
-  codeDefineLimits <- readChar(fileName, file.info(fileName)$size)
-  eval(parse(text = codeDefineLimits), .GlobalEnv)
+
+  # An example DefineParameterLimits.R file can be found in
+  # vignettes/DefineParameterLimits.R of the PCMFit package source-code.
+  # Note that the path here is relative to the working directory of
+  # the worker node R-process.
+  source('../../DefineParameterLimits.R', local = FALSE)
 }
 
-tree <- PCMTree(PCMFitDemoObjects$dtSimulated$tree[[1]])
-X <- PCMFitDemoObjects$valuesBootstrapMGPM_A_F_BC2_RR$X[[data_id]][
-  , seq_len(PCMTreeNumTips(tree))]
+# Inferred best model (
+# replace this with the result from a previous call to  RetrieveBestFitScore(fitObject)$inferredModel)
+bestModel <-
+  RetrieveBestFitScore(PCMFitDemoObjects$fitMGPM_A_F_BC2_RR)$inferredModel
+# Inferred tree
+tree <- attr(bestModel, "tree")
+# Simulate parameteric bootstrap trait values
+X <- PCMSim(tree, bestModel, bestModel$X0)[, seq_len(PCMTreeNumTips(tree))]
 
 currentResultFile <- paste0("CurrentResults_fits_", prefixFiles, ".RData")
 if(file.exists(currentResultFile)) {
--- vignettes/FitBootstrap_MGPM_A_F_BC2_RR_bsub.sh
+++ vignettes/FitBootstrap_MGPM_A_F_BC2_RR_bsub.sh
@@ -7,7 +7,10 @@ then
 rm MPI*.log
 rm CurrentResults*.RData
 else
-bsub -M 10000 -n 8 -W 3:59 -R ib sh R --vanilla --slave -f ../../FitBootstrap_MGPM_A_F_BC2_RR.R --args $id
+# The following would execute only if the bsub command is available on your system
+# bsub -M 10000 -n 8 -W 3:59 -R ib R --vanilla --slave -f ../../FitBootstrap_MGPM_A_F_BC2_RR.R --args $id
+# Should run on unix systems. Comment or remove this line if uncommenting the above.
+R --vanilla --slave -f ../../FitBootstrap_MGPM_A_F_BC2_RR.R --args $id
 fi
 cd ../..
 done
